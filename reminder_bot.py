da44f7a1f901e27cca63fe188fbb2c5e340ea2a2
ultima333 <ultima3333@gmail.com>
2025-01-31 20:14:10 +0300

    Повернені інтервали , змінено низький пріорітет

--- reminder_bot.py
+++ reminder_bot.py
@@ -258,13 +258,13 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
         if priority == 'urgent':
             existing_jobs = context.job_queue.get_jobs_by_name(f'urgent_{assigned_user}')
             if not any(job.data[0] == assigned_user and job.data[1] == priority for job in existing_jobs):
-                context.job_queue.run_repeating(remind_task, interval=60, first=0, chat_id=assigned_user, data=(assigned_user, priority), name=f'urgent_{assigned_user}')
+                context.job_queue.run_repeating(remind_task, interval=3600, first=0, chat_id=assigned_user, data=(assigned_user, priority), name=f'urgent_{assigned_user}')
         elif priority == 'medium':
             existing_jobs = context.job_queue.get_jobs_by_name(f'medium_{assigned_user}')
             if not any(job.data[0] == assigned_user and job.data[1] == priority for job in existing_jobs):
-                context.job_queue.run_repeating(remind_task, interval=120, first=0, chat_id=assigned_user, data=(assigned_user, priority), name=f'medium_{assigned_user}')
+                context.job_queue.run_repeating(remind_task, interval=21600, first=0, chat_id=assigned_user, data=(assigned_user, priority), name=f'medium_{assigned_user}')
         elif priority == 'low':
-            reminder_time = time(7, 0, 0)
+            reminder_time = time(22, 2, 5)
             existing_jobs = context.job_queue.get_jobs_by_name(f'low_{assigned_user}')
             if not any(job.data[0] == assigned_user and job.data[1] == priority for job in existing_jobs):
                 context.job_queue.run_daily(remind_task, time=reminder_time, chat_id=assigned_user, data=(assigned_user, priority), name=f'low_{assigned_user}')
